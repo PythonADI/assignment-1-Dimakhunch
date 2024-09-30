c4250329701eb80ddab5ca77907472c06c13c750
Dimakhunch <dima.khunch@gmail.com>
2024-09-30 22:21:43 +0400

    Homework done main.py

--- homework1/main.py
+++ homework1/main.py
@@ -1,5 +1,17 @@
 def main() -> None:
     # Write your solution here
+print ('Hello Word')
+print ('5+7=', 5+7)
+print ('37-8=', 37-8)
+print ('-90*4=', -90*4)
+print ('100/3=', 100/3)
+print ('0.2+0.1=', 0.2+0.1)
+print ('7.068 * 4.3=', 7.068 * 4.3)
+print ('8 ** 2=', 8 ** 2)
+print ('9 ** 3=', 9 ** 3)
+print ('5 / 3=', 5 / 3)
+print ('5 // 3=', 5 // 3)
+print ('99 % 7=', 99 % 7)
     pass
 
 
